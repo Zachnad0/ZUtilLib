3a1dd7c1ca851cdf5ee25649da4035ff1431d3ce
Zachnad0 <zjhobbs@gmail.com>
2023-07-06 13:15:46 +1200

    ADDED core solution and c# project

---
+++ UnityTools.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace ZUtilLib
+{
+	public static class UnityTools
+	{
+		public static Quaternion ToQuaternion(this Vector3 v)
+		{
+			return Quaternion.LookRotation(v, Vector3.Scale(v, Vector3.up));
+		}
+		public static Vector3 ToVector3(this Quaternion q)
+		{
+			return Vector3.Normalize(q * Vector3.forward);
+		}
+	}
+}
