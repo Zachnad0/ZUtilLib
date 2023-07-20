01028f05528fc1f69919cf4f0d2774b2a8f96cce
Zachnad0 <zjhobbs@gmail.com>
2023-07-20 18:37:24 +1200

    Remove unity engine requirements. Unity extras are now an additional DLL which must be added too.

---
+++ GenericUtils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZUtilLib
+{
+	public static class GenericUtils
+	{
+		public delegate void PromiseFormat();
+
+		public static async void Then(this Task task, PromiseFormat promise)
+		{
+			await task;
+			if (promise != null)
+				promise();
+			else
+				throw new Exception("Then missing promise method!");
+		}
+	}
+}
