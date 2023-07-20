46ef99ee6f8f39c457166e98c0c6befc6728db72
Zachnad0 <zjhobbs@gmail.com>
2023-07-20 20:54:56 +1200

    Bruh make the JS imitation promise .then() system work man just so bamboozling 100%

--- GenericUtils.cs
+++ GenericUtils.cs
@@ -7,15 +7,17 @@ namespace ZUtilLib
 {
 	public static class GenericUtils
 	{
-		public delegate void PromiseFormat();
+		public delegate TOut PromiseFormat<TOut, TIn>(TIn inputData);
 
-		public static async void Then(this Task task, PromiseFormat promise)
+		public static async Task<TOutput> Then<TOutput, TInput>(this Task<TInput> task, PromiseFormat<TOutput, TInput> promise)
 		{
-			await task;
+			TInput result = await task; // CONTINUE HERE OR SOMETHING JUST MAKE IT WORK
 			if (promise != null)
-				promise();
+			{
+				return promise(result);
+			}
 			else
-				throw new Exception("Then missing promise method!");
+				throw new Exception("Missing promise method!");
 		}
 	}
 }
