b31e4d8f3b37b58c09fa993d221ccdd718bea07a
Zachnad0 <zjhobbs@gmail.com>
2023-07-31 20:35:46 +1200

    Adjusted entire csproj and solution crap bruh

--- GenericUtils.cs
+++ GenericUtils.cs
@@ -1,23 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
+using System.ComponentModel;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace ZUtilLib
 {
-	public static class GenericUtils
+	public static class ZUtils
 	{
-		public delegate TOut PromiseFormat<TOut, TIn>(TIn inputData);
-
-		public static async Task<TOutput> Then<TOutput, TInput>(this Task<TInput> task, PromiseFormat<TOutput, TInput> promise)
+		/// <summary>
+		/// Loads the given JSON file and deserializes it.
+		/// </summary>
+		/// <typeparam name="T">Type to be parsed.</typeparam>
+		/// <param name="path">Path of file</param>
+		/// <returns></returns>
+		public static async Task<(bool, T)> LoadJSONFromFile<T>(string path, JsonSerializerOptions options)
 		{
-			TInput result = await task; // CONTINUE HERE OR SOMETHING JUST MAKE IT WORK
-			if (promise != null)
+			if (File.Exists(path))
 			{
-				return promise(result);
+				using (FileStream fileStream = File.OpenRead(path))
+				{
+					try
+					{
+						T result = await JsonSerializer.DeserializeAsync<T>(fileStream, options);
+						return (true, result);
+					}
+					catch
+					{
+					}
+				}
+
 			}
-			else
-				throw new Exception("Missing promise method!");
+
+			return (false, default);
+		}
+
+		/// <summary>
+		/// Loads the given JSON file, then overwrites it with the input data.
+		/// </summary>
+		/// <param name="thing">The "thing" to be serialized</param>
+		/// <param name="path">Path of file</param>
+		/// <param name="doNotOverwrite">Set to true if instead the data should be appended</param>
+		/// <returns>If the operation was successful</returns>
+		public static async Task<bool> WriteJSONToFile<T>(T thing, string path, JsonSerializerOptions options, bool doNotOverwrite = false)
+		{
+			if (File.Exists(path))
+			{
+				using (FileStream fileStream = File.OpenWrite(path))
+				{
+					if (!doNotOverwrite)
+					{
+						await JsonSerializer.SerializeAsync(fileStream, thing, options);
+					}
+					else
+					{
+						string output = JsonSerializer.Serialize(thing, options);
+						await fileStream.WriteAsync(Encoding.UTF8.GetBytes(output), (int)fileStream.Length, int.MaxValue);
+					}
+					return true;
+				}
+			}
+
+			return false;
 		}
 	}
 }
