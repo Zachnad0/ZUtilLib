125f2b482cf38c1f8db10b3eadabf3f56c528fd8
Zachnad0 <zjhobbs@gmail.com>
2023-08-08 12:12:47 +1200

    WIP neural network cloning and generation

--- ZAI.cs
+++ ZAI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Security.Cryptography;
+using System;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
 using System.Linq;
@@ -6,12 +7,12 @@ using System.Linq;
 namespace ZUtilLib.ZAI // Random AI stuff here
 {
 	/// <summary>
-	/// A singular neural network instance
+	/// A neural network instance
 	/// </summary>
 	public class NeuralNetwork
 	{
 		[JsonPropertyName("input_layer")]
-		public NeuralDataUnit[] InputLayer { get; private set; }
+		public NeuralDataUnit[] InputLayer { get; private set; } // These have actually gotta be values
 
 		[JsonPropertyName("output_layer")]
 		public NeuralDataUnit[] OutputLayer { get; private set; }
@@ -28,20 +29,46 @@ namespace ZUtilLib.ZAI // Random AI stuff here
 			InputLayer = new NeuralDataUnit[inputHeight];
 			OutputLayer = new NeuralDataUnit[outputHeight];
 			InternalLayers = new NeuralDataUnit[internalCount, internalHeights];
+		}
 
-			// Generate NeuralDataUnits
-			for (int i = 0; i < inputHeight; i++)
+		[JsonConstructor]
+		public NeuralNetwork(NeuralDataUnit[,] internalLayers)
+		{
+			// Fill layers from saved data
+			InternalLayers = internalLayers;
+		}
+
+		public void InitializeThis(NeuralNetwork basedOn = null)
+		{
+			if (basedOn == null)
 			{
-				//CONTINUE HERE =====================================================================
+				// CONTINUE HERE ===========================================================
+				return;
+			}
+
+			if (basedOn.InputLayer.Length == InputLayer.Length && basedOn.InternalLayers.Length == InternalLayers.Length && basedOn.OutputLayer.Length == OutputLayer.Length)
+			{ // Ensure it's sizing is identical
+				return;
 			}
 		}
 	}
+}
 
-	/// <summary>
-	/// I mean yea it is called a neuron, but uh skill issue
-	/// </summary>
-	public class NeuralDataUnit
+/// <summary>
+/// I mean yea it is called a neuron, but uh skill issue
+/// </summary>
+public class NeuralDataUnit
+{
+	[JsonInclude]
+	[JsonPropertyName("neuron_weight")]
+	public readonly float InternalWeight;
+
+	[JsonPropertyName("input_neurons")]
+	public NeuralDataUnit[] InputDataUnits { get; private set; }
+
+	[JsonConstructor]
+	public NeuralDataUnit(float neuronValue)
 	{
-
+		InternalWeight = neuronValue;
 	}
 }
